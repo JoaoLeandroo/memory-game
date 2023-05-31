6609916ee93cfa314d845b7b83047eca466c2603
joaoleandro <new.joaoleandro@gmail.com>
2023-05-31 11:53:32 -0300

    adicionado script para sortear numeros aleatorios

--- src/script/script.js
+++ src/script/script.js
@@ -1,12 +1,35 @@
 
-const buttonStart = document.querySelector(".start-game button")
-const sectionStart = document.querySelector(".start-game")
-const cardsGame = document.querySelector(".cards-game")
+// const buttonStart = document.querySelector(".start-game button")
+// const sectionStart = document.querySelector(".start-game")
+// const cardsGame = document.querySelector(".cards-game")
 
-// const randomCards = ['']
-buttonStart.addEventListener("click", function() {
-    sectionStart.style.display = 'none'
-    cardsGame.style.display = 'grid'
 
-})
+// buttonStart.addEventListener("click", function() {
+//     sectionStart.style.display = 'none'
+//     cardsGame.style.display = 'grid'
 
+// })
+
+
+function sortearNumeros() {
+    let numerosSorteados = [];
+    
+    for (let i = 0; i < 5; i++) {
+      let numeroAleatorio;
+      let repetido;
+      
+      do {
+        numeroAleatorio = Math.floor(Math.random() * 27) + 1;
+        repetido = numerosSorteados.includes(numeroAleatorio);
+      } while (repetido);
+      
+      numerosSorteados.push(numeroAleatorio);
+    }
+    
+    return numerosSorteados;
+  }
+  
+  let numerosSorteados = sortearNumeros();
+  
+  // Exibindo os números sorteados
+  console.log(numerosSorteados);
