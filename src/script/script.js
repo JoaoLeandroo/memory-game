f9bad3a554f507328098685d80db78fd68c543c5
joaoleandro <new.joaoleandro@gmail.com>
2023-06-01 11:52:05 -0300

    estudando melhor o codigo

--- src/script/script.js
+++ src/script/script.js
@@ -1,89 +1,45 @@
-const memoryGame = document.querySelector(".game-memory")
-const buttonResult = document.querySelector(".result-game")
+const gameMemory = document.querySelector(".game-memory")
 
-const cartas = [
+const characters =  [
     '1',
     '2',
     '3',
     '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    '11',
-    '12',
-    '13',
-    '14',
-    '15',
-    '16',
-    '17',
-    '18',
-    '19',
-    '20',
-    '21',
-    '22',
-    '23',
-    '24',
-    '25',
-    '26',
-    '27'
+    '5'
 ]
 
-// buttonResult.addEventListener('click', () => {
-//     numerosSorteados
-//     console.log(numerosSorteados)
+const createElement = (tag, className) => {
+    const element = document.createElement(tag)
+    element.className = className
+    return element
+}
 
-// })
+const createCard = (character) => {
+    const card = createElement('div', 'card')
+    const front = createElement('div', 'face front')
+    const back = createElement('div', 'face back')
+    
+    front.style.backgroundImage = `url(../../src/assets/images-card/${character}.png)`
 
-function createCard() {
-    let num = sortearNumeros()
-    for(let i=0; i < 5; i++) {
-        const card = document.createElement("div")
-        const front = document.createElement("div")
-        const back = document.createElement("div")
-        const img = document.createElement('img')
-        img.src = `../../src/assets/images-card/${cartas[num[i]]}.png`
+    card.appendChild(front)
+    card.appendChild(back)
 
-
-        card.className = 'card'
-        front.className = 'face front'
-        back.className = 'face back'
-
-        front.appendChild(img)
-        card.appendChild(front)
-        card.appendChild(back)
-
-        memoryGame.appendChild(card)
-    }
+    return card
 }
 
 createCard()
 
 
+const loadGame = () => {
 
-
-
-function sortearNumeros() {
-    let numerosSorteados = [];
+    const duplicateCharacters = [ ...characters, ...characters ]
     
-    for (let i = 0; i < 5; i++) {
-      let numeroAleatorio;
-      let repetido;
-      
-      do {
-        numeroAleatorio = Math.floor(Math.random() * 27) + 1;
-        repetido = numerosSorteados.includes(numeroAleatorio);
-      } while (repetido);
-      
-      numerosSorteados.push(numeroAleatorio);
-    }
-    
-    return numerosSorteados;
-  }
-  
-  let numerosSorteados = sortearNumeros();
-  
-  // Exibindo os números sorteados
-  console.log(numerosSorteados);
+    const embaralhar = duplicateCharacters.sort(() => Math.random() - 0.5)
+
+    embaralhar.forEach((character) => {
+        const card = createCard(character);
+        gameMemory.appendChild(card)
+    })
+}
+
+loadGame()
